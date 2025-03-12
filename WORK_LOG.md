10c1b366d3d5ea1b9a8598f69477865250523ef0
lkrenz <liamkrenz100@gmail.com>
2025-03-12 14:59:15 -0700

    Started coding tabulation solution. I think it works, but I need to correctly order the solutions.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date     |   Time    |                                                                                                                                                                                          Update |
+|:---------|:---------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5  |  9-10pm   |                                                                                                                                                      I tried to write abstract, efficient code. |
+| March 12 | 2:30-3:00 | I spent this time implementing the the tabulation solution to this problem--the cases of checking if characters match, then up, left, and diagonal for addition, subtraction, and substitution. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Autocorrect
@@ -12,13 +14,39 @@ import java.io.IOException;
  */
 public class Autocorrect {
 
+    String[] words;
+    int threshold;
     /**
      * Constucts an instance of the Autocorrect class.
      * @param words The dictionary of acceptable words.
      * @param threshold The maximum number of edits a suggestion can have.
      */
     public Autocorrect(String[] words, int threshold) {
+        this.words = words;
+        this.threshold = threshold;
+    }
 
+    public int findEditDistance(String word1, String word2) {
+        int[][] paths = new int[word1.length() +1][word2.length()+1];
+        word1 = " " + word1;
+        word2 = " " + word2;
+
+        for (int j = 0; j < word2.length(); j++) {
+            paths[0][j] = j;
+        }
+        for (int i = 1; i < word1.length(); i++) {
+            paths[i][0] = i;
+            for (int j = 1; j < word2.length(); j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    paths[i][j] = paths[i - 1][j -1];
+                }
+                else {
+                    paths[i][j] = Math.min(Math.min(paths[i-1][j], paths[i][j-1]), paths[i-1][j-1]) + 1;
+                }
+            }
+        }
+
+        return paths[word1.length() -1][word2.length() -1];
     }
 
     /**
@@ -28,8 +56,16 @@ public class Autocorrect {
      * to threshold, sorted by edit distnace, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
+        ArrayList<String> closeWords = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (findEditDistance(typed, this.words[i]) <= threshold) {
+                closeWords.add(words[i]);
+            }
+        }
+        String[] words = closeWords.toArray(new String[0]);
+        System.out.println(Arrays.toString(words));
 
-        return new String[0];
+        return closeWords.toArray(new String[0]);
     }
 
 
